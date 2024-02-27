d82267b67f90093fe0b916a87cae2231868f623a
Annateresa01 <shajianna013@gmail.com>
2024-02-27 15:19:07 +0530

    ok

--- controller/member.js
+++ controller/member.js
@@ -67,6 +67,34 @@ router.get("/MemberDetails", async (req, res) => {
     }
 });
 
+router.get("/search",async(req,res)=>{
+    let input=req.body
+    let name=req.body.name
+    let data=await memberModel.find({"name":name})
+   
+    if (!data ||data.length === 0) {
+        return res.json({
+            status:"Invalid user"
+        })
+    }
+    else{
+        const responseData = data.map(user => ({
+            name: user.name,
+            address: user.address,
+            weight: user.weight,
+            height: user.height,
+            idproof: user.idproof,
+            emailid: user.emailid,
+            contactno: user.contactno
+        }))
+
+        console.log(responseData);
+
+        return res.json(responseData);
+    } 
+    
+})
+
 
 
 
