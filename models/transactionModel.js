5ccef999bfdb8a4b4846320705fb4143f3e97598
ar7unms <msarjun077@gmail.com>
2024-02-27 23:13:15 +0530

    hh

--- models/transactionModel.js
+++ models/transactionModel.js
@@ -1,23 +1,23 @@
-const mongoose =require("mongoose")
+// const mongoose =require("mongoose")
 
-const tranSchema=new mongoose.Schema(
-    {
-        userid:{
-            type:mongoose.Schema.Types.ObjectId,
-            required:true,
-            ref:"usergym"
-        },
-        packageid:{
-            type:mongoose.Schema.Types.ObjectId,
-            required:true,
-            ref:"package"
-        },
-        postdate:{
-            type:Date,
-            default:Date.now
-        },
+// const tranSchema=new mongoose.Schema(
+//     {
+//         userid:{
+//             type:mongoose.Schema.Types.ObjectId,
+//             required:true,
+//             ref:"usergym"
+//         },
+//         packageid:{
+//             type:mongoose.Schema.Types.ObjectId,
+//             required:true,
+//             ref:"package"
+//         },
+//         postdate:{
+//             type:Date,
+//             default:Date.now
+//         },
 
-    }
-)
-module.exports=mongoose.model('transaction',tranSchema)
+//     }
+// )
+// module.exports=mongoose.model('transaction',tranSchema)
 
