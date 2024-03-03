eeaa76c21dd1ed86da1f13c229a10d86241f73b5
Annateresa01 <shajianna013@gmail.com>
2024-03-03 19:36:00 +0530

    ok

--- controller/adminRoute.js
+++ controller/adminRoute.js
@@ -21,6 +21,7 @@ router.post("/update",async(req,res)=>{
     res.json({
         status:"success"
     })
+ 
 })
 
 router.post("/delete",async(req,res)=>{
@@ -28,6 +29,15 @@ router.post("/delete",async(req,res)=>{
     let {id} = req.body
     console.log(id)
     let data = await memberModel.deleteOne({_id:id})
+    res.json({
+        status:"success"
+    })
+})
+router.post("/updateAttendence",async(req,res)=>{
+    console.log(req.body)
+    let {id,...rest} = req.body
+    console.log(rest)
+    let data = await memberModel.updateOne({_id:id},rest)
     res.json({
         status:"success"
     })
--- models/memberModel.js
+++ models/memberModel.js
@@ -54,7 +54,8 @@ const userSchema=new mongoose.Schema(
             type:String, 
             required:true
         },
-        paymentStatus:String
+        paymentStatus:String,
+        attendence:String
     }
 )
 
