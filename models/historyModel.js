9c845f67b4cd16391ddb512c45883ee1a1709167
lakshmiibalu <lakshmibalu74@gmail.com>
2024-03-06 13:24:38 +0530

    new

--- models/historyModel.js
+++ models/historyModel.js
@@ -1,31 +1,3 @@
-// // updateHistoryModel.js
-
-// const mongoose = require('mongoose');
-
-// const updateHistorySchema = new mongoose.Schema({
-//     userId: {
-//         type : mongoose.Schema.Types.ObjectId,
-//             required : true,
-//             ref : "usergym"
-//     },
-//     oldPackageId: {
-//         type : mongoose.Schema.Types.ObjectId,
-        
-//             ref : "packages"
-//     },
-//     newPackageId: {
-//         type : mongoose.Schema.Types.ObjectId,
-//             required : true,
-//             ref : "packages"
-//     },
-//     updatedAt: {
-//         type: Date,
-//         default: Date.now
-//     }
-// });
-
-// module.exports = mongoose.model('UpdateHistory', updateHistorySchema);
-
 
 
 const mongoose = require('mongoose');
